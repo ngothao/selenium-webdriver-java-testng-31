814e746ffa3551501744ef7b4ab2e44a080f154b
ngothithao <ngothao.tm@gmail.com>
2024-07-15 23:32:27 +0700

    Homework

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -26,22 +26,46 @@ public class Topic_01_Check_Environment {
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("https://www.facebook.com/");
+        driver.get("https://alada.vn/tai-khoan/dang-ky.html");
     }
 
     @Test
-    public void TC_01_Url() {
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
-    }
+    public void TC_01_Empty_Data()
+    {
+        driver.get("https://alada.vn/tai-khoan/dang-ky.html");
 
-    @Test
-    public void TC_02_Logo() {
-        Assert.assertTrue(driver.findElement(By.cssSelector("img.fb_logo")).isDisplayed());
-    }
+      driver.findElement(By.id("txtFirstname")).clear();
+      driver.findElement(By.id("txtEmail")).clear();
+      driver.findElement(By.id("txtCEmail")).clear();
+      driver.findElement(By.id("txtPassword")).clear();
+      driver.findElement(By.id("txtCPassword")).clear();
+      driver.findElement(By.id("txtPhone")).clear();
 
+      driver.findElement(By.xpath("//button[text()='ĐĂNG KÝ' and @type='submit']")).click();
+
+      Assert.assertEquals(driver.findElement(By.id("txtFirstname-error")).getText(),"Vui lòng nhập họ tên");
+      Assert.assertEquals(driver.findElement(By.id("txtEmail-error")).getText(),"Vui lòng nhập email");
+      Assert.assertEquals(driver.findElement(By.id("txtCEmail-error")).getText(),"Vui lòng nhập lại địa chỉ email");
+      Assert.assertEquals(driver.findElement(By.id("txtPassword-error")).getText(),"Vui lòng nhập mật khẩu");
+      Assert.assertEquals(driver.findElement(By.id("txtCPassword-error")).getText(),"Vui lòng nhập lại mật khẩu");
+      Assert.assertEquals(driver.findElement(By.id("txtPhone-error")).getText(),"Vui lòng nhập số điện thoại.");
+    }
     @Test
-    public void TC_03_Form() {
-        Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
+    public void TC_02_Invalid_Email()
+    {
+        driver.get("https://alada.vn/tai-khoan/dang-ky.html");
+
+        driver.findElement(By.id("txtFirstname")).sendKeys("Ngo thi Thao");
+        driver.findElement(By.id("txtEmail")).sendKeys("thao@@gmail.com");
+        driver.findElement(By.id("txtCEmail")).sendKeys("thao@@@gmail.com");
+        driver.findElement(By.id("txtPassword")).sendKeys("123@abc");
+        driver.findElement(By.id("txtCPassword")).sendKeys("123@abc");
+        driver.findElement(By.id("txtPhone")).sendKeys("0977109156");
+
+        driver.findElement(By.xpath("//button[text()='ĐĂNG KÝ' and @type='submit']")).click();
+
+        Assert.assertEquals(driver.findElement(By.id("txtEmail-error")).getText(),"Vui lòng nhập email hợp lệ");
+        Assert.assertEquals(driver.findElement(By.id("txtCEmail-error")).getText(),"Email nhập lại không đúng");
     }
 
     @AfterClass
